d82fc79e716a1ecab5f228e28b9c74e5e2cdd6f9
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-05-07 08:07:14 +0100

    compiler errors

--- src/ATM2.hs
+++ src/ATM2.hs
@@ -67,14 +67,34 @@ atm = InsertCard
       :>> EjectCard
       :>> Message "Bye"
 
--- runATM :: Cmd a s1 s2 -> IO a
--- runATM InsertCard      = putStrLn "Insert card and print enter" >> void getLine
--- runATM EjectCard       = putStrLn "Card ejected"
--- runATM GetPIN          = putStrLn "Enter pin:" >> getLine
--- -- runATM (CheckPIN pin) = return $ if pin == "1234" then SCorrectPIN else SWrongPIN
--- runATM GetAmount       = read <$> (putStrLn "Enter amount:" >> getLine)
--- runATM (Dispense cash) = putStrLn $ "Here is " ++ show cash
--- runATM (Message msg)   = putStrLn msg
--- runATM (Return res)    = return res
--- runATM (c1 :>> c2)     = runATM c1 >> runATM c2
+runATM :: Cmd a s1 s2 -> IO a
+runATM InsertCard      = putStrLn "Insert card and print enter" >> void getLine
+runATM EjectCard       = putStrLn "Card ejected"
+runATM GetPIN          = putStrLn "Enter pin:" >> getLine
+-- runATM (CheckPIN pin) = return $ if pin == "1234" then SCorrectPIN else SWrongPIN
+runATM GetAmount       = read <$> (putStrLn "Enter amount:" >> getLine)
+runATM (Dispense cash) = putStrLn $ "Here is " ++ show cash
+runATM (Message msg)   = putStrLn msg
+runATM (Return res)    = return res
+-- below does not type check
+-- • Couldn't match kind ‘k1’ with ‘Type’
+-- ‘k1’ is a rigid type variable bound by
+-- a pattern with constructor:
+--   :>> :: forall k1 k2 (a :: k1) (s1 :: ATMState) (s2 :: ATMState)
+--                 (b :: k2) (s3 :: ATMState).
+--          Cmd a s1 s2 -> Cmd b s2 s3 -> Cmd b s1 s3,
+-- in an equation for ‘runATM’
+-- at src/ATM2.hs:79:9-17
+-- When matching types
+-- a0 :: Type
+-- a1 :: k1
+-- Expected type: IO a0
+-- Actual type: IO a1
+-- • In the first argument of ‘(>>)’, namely ‘runATM c1’
+-- In the expression: runATM c1 >> runATM c2
+-- In an equation for ‘runATM’:
+--   runATM (c1 :>> c2) = runATM c1 >> runATM c2
+-- • Relevant bindings include
+-- c1 :: Cmd a1 s1 s4 (bound at src/ATM2.hs:79:9)
+runATM (c1 :>> c2)     = runATM c1 >> runATM c2
 -- -- runATM (c :>>= f)      = runATM c >>= \x -> runATM (f x)
